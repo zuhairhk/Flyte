85055f778d740848b368bc6ba9c4f52de02cb215
zuhairhk <zuhairhk@gmail.com>
2021-09-06 16:45:07 -0400

    Initial Commit

---
+++ Jetpack Perky Shooter/Assets/Scripts/Gun.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public float damage = 10f;
+    public float range = 100f;
+
+    public GameObject go;
+
+    public Camera fpsCam;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+
+            Target target = hit.transform.GetComponent<Target>();
+            if (target != null)
+            {
+                target.TakeDamage(damage);
+            }
+        }
+
+    }
+
+    /*
+    void LateUpdate()
+    {
+        Transform target = Camera.main.transform;
+        go.transform.parent = target;
+    }
+    */
+}
